a89f55bf0194b25f04153278b507a6ac2ac22dbe
Nephthali Salam <snephthali@gmail.com>
2025-05-11 01:45:51 +0200

    Add Category in CreateItemDTO

--- src/modules/item/types/CreateItemDTO.ts
+++ src/modules/item/types/CreateItemDTO.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { ItemType } from "./ItemType";
-import { ItemStatus } from "./ItemStatus";
+import { CategoryType } from "./CategoryType";
 
 export interface CreateItemDTO {
     supplierId: mongoose.Types.ObjectId;
     name: string;
     type: ItemType;
+    category: CategoryType;
     quantity: number;
     bestBefore: Date;
     readyBy: Date;
